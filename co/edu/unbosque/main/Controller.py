befd5f5b28990a8a6831a8fe9c51bd0c535aba90
Briannys Monserrate <bpaezm@unbosque.edu.co>
2021-05-25 18:32:45 -0500

    class treeavl y node

--- co/edu/unbosque/main/Controller.py
+++ co/edu/unbosque/main/Controller.py
@@ -1,3 +1,14 @@
 class Controller:
     def __init__(self):
         print("Esta funcionando bien")
+        myTree = AVLTree()
+        root = None
+        nums = [33, 13, 52, 9, 21, 61, 8, 11]
+        for num in nums:
+            root = myTree.insert_node(root, num)
+        myTree.printHelper(root, "", True)
+        key = 13
+        root = myTree.delete_node(root, key)
+        print("After Deletion: ")
+        myTree.printHelper(root, "", True)
+
